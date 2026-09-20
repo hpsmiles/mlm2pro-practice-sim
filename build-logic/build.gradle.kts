7287044946dbab5b69acf7a291daaf893b46db42
Harry Smiles <harry.smiles@gmail.com>
2026-09-20 14:10:29 +1000

    build: convention plugin and core-designsystem module

---
+++ build-logic/build.gradle.kts
@@ -0,0 +1,10 @@
+// build-logic/build.gradle.kts
+plugins {
+    `kotlin-dsl`
+}
+
+dependencies {
+    compileOnly(libs.android.gradlePlugin)
+    compileOnly(libs.kotlin.gradlePlugin)
+    compileOnly(libs.kotlin.compose.gradlePlugin)
+}
---
+++ build-logic/src/main/kotlin/golf-android-library.gradle.kts
@@ -0,0 +1,27 @@
+// build-logic/src/main/kotlin/golf-android-library.gradle.kts
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    compileSdk = 37
+    defaultConfig {
+        minSdk = 31
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
